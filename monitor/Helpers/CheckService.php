14d7fb4c96d3cec09cc30fc50894f69ddddde999
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-17 23:14:05 +0300

    The first prototype of Checker

--- monitor/Helpers/CheckService.php
+++ monitor/Helpers/CheckService.php
@@ -4,5 +4,37 @@ namespace Monitor\Helpers;
 
 class CheckService
 {
+    private $ch;
+    private $executionResult;
 
+    public function __construct($url)
+    {
+        $this->ch = curl_init($url);
+
+        curl_setopt($this->ch, CURLOPT_HEADER, true);
+        curl_setopt($this->ch, CURLOPT_NOBODY, true);
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($this->ch, CURLOPT_TIMEOUT,5);
+
+        $this->executionResult = $this->executeCh();
+    }
+
+    public function executeCh() {
+        return curl_exec($this->ch);
+    }
+
+    public function getResponseHttpCode()
+    {
+        return curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+    }
+
+    public function getResponseSize()
+    {
+        //return curl_getinfo($this->ch, CURLINFO_REQUEST_SIZE);
+    }
+
+    public function getResponseTime()
+    {
+        return curl_getinfo($this->ch, CURLINFO_TOTAL_TIME);
+    }
 }
