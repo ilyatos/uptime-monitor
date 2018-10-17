103b49dacc5080a635480905ab5ca9b1b2c0fa0a
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-17 23:11:55 +0300

    Add methods forfuture implementation

--- monitor/Entities/Service.php
+++ monitor/Entities/Service.php
@@ -6,8 +6,23 @@ use Core\BaseEntity;
 
 class Service extends BaseEntity
 {
-    public static function get()
+    public static function all()
     {
         $connection = self::getConnection();
+
+        $stmt = $connection->prepare("SELECT * FROM `services`");
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+
+    public static function store()
+    {
+        
+    }
+
+    public static function updateWhere()
+    {
+        
     }
 }
