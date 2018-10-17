4e352d968b41fbe1faaa1651ad5fc9795fab2bde
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-17 11:17:17 +0300

    Trait to get connection to the database

---
+++ Core/Traits/DBConnectionTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Core\Traits;
+
+use PDO;
+
+trait ConnectionTrait {
+
+    /**
+     * Get the PDO database connection.
+     *
+     * @return PDO
+     */
+    protected static function getConnection() {
+        static $connection;
+
+        if ($connection === null) {
+
+            $config = require ROOT . '/Core/config/db.php';
+
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            ];
+
+            try {
+                $connection = new PDO(
+                    sprintf("mysql:dbname=%s; host=%s; charset=utf8", $config['name'], $config['host']),
+                    $config['user'],
+                    $config['password'],
+                    $options);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+                die;
+            }
+        }
+
+        return $connection;
+    }
+}
