ae16314bc64dd7c29f44bb6a2b39d419e7d5ff31
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-17 18:18:40 +0300

    Created the enter point

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use Pecee\SimpleRouter\SimpleRouter;
+
+define('ROOT', dirname(__DIR__));
+
+/**
+* Autoloader
+*/
+require_once ROOT . '/vendor/autoload.php';
+
+/**
+ * Routing
+ */
+require_once ROOT . '/vendor/pecee/simple-router/helpers.php';
+require_once ROOT . '/monitor/routes.php';
