0ca3b3130f4f4d76632837fdd52a61e8bb6d417b
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-18 20:36:18 +0300

    Change namespaces

--- deploy.php
+++ deploy.php
@@ -2,8 +2,8 @@
 
 require_once dirname(__FILE__) . '/vendor/autoload.php';
 
-use Monitor\Database\CreateTables;
-use Monitor\Database\SeedTables;
+use App\Database\CreateTables;
+use App\Database\SeedTables;
 
 /**
  * Create a database, enter login, password, host, db_name
