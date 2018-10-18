fa42cd53a685ae03c435d3bbf1c4ca10eeca88fc
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-18 11:41:32 +0300

    Add Select all (* or columns) and updateWhere

--- monitor/Entities/Service.php
+++ monitor/Entities/Service.php
@@ -6,23 +6,49 @@ use Core\BaseEntity;
 
 class Service extends BaseEntity
 {
-    public static function all()
+    public static function all(array $columns = [])
     {
         $connection = self::getConnection();
 
-        $stmt = $connection->prepare("SELECT * FROM `services`");
+        $sql = '';
+
+        if (!empty($columns)) {
+            foreach ($columns as $column) {
+                $sql = $sql . $column . ',';
+            }
+        } else {
+            $sql = '*';
+        }
+
+        $sql = rtrim($sql, ',');
+
+        $stmt = $connection->prepare("SELECT $sql FROM `services`");
         $stmt->execute();
 
         return $stmt->fetchAll();
     }
 
-    public static function store()
+    public static function store(array $values)
     {
         
     }
 
-    public static function updateWhere()
+    public static function updateWhere(array $updatedValues, string $column, string $param)
     {
-        
+        $connection = static::getConnection();
+
+        $sql = '';
+
+        foreach ($updatedValues as $key => $value) {
+            $sql = $sql . $key . '=' . '\'' . $value . '\'' . ',';
+        }
+
+        $sql = rtrim($sql, ',');
+
+        $query = $connection->prepare("UPDATE `services` SET $sql WHERE $column=:p");
+
+        $query->bindParam(':p', $param);
+
+        return $query->execute();
     }
 }
