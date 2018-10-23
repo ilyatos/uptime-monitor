b793242b2e6daf73dab9b53c168bc258823fcfcc
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-23 19:36:28 +0300

    Move Response logic to sepparate class

---
+++ app/monitor/Helpers/ResponseFromService.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Monitor\Helpers;
+
+
+class ResponseFromService
+{
+    private $executionResult;
+    private $executedCurl;
+
+    public function __construct($executionResult, $executedCurl)
+    {
+        $this->executionResult = $executionResult;
+        $this->executedCurl = $executedCurl;
+    }
+
+    /**
+     * Get response http code.
+     *
+     * @return int
+     */
+    public function getHttpCode(): int
+    {
+        return curl_getinfo($this->executedCurl, CURLINFO_HTTP_CODE);
+    }
+
+    /**
+     * Get response time.
+     *
+     * @return float
+     */
+    public function getTime(): float
+    {
+        return curl_getinfo($this->executedCurl, CURLINFO_TOTAL_TIME);
+    }
+
+    /**
+     * Get response size.
+     *
+     * @return int
+     */
+    public function getSize(): int
+    {
+        return curl_getinfo($this->executedCurl, CURLINFO_SIZE_DOWNLOAD);
+    }
+}
